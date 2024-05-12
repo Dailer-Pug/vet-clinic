1443e159ae8cbaf7be1e04ca9014b43e090c855c
dailer <Derrius@mail.ru>
2024-05-12 20:10:55 +0400

    change some styles for components

--- src/components/coreComponents/RadioInput/TabStyle.tsx
+++ src/components/coreComponents/RadioInput/TabStyle.tsx
@@ -6,11 +6,11 @@ export const TabStyle = styled.div`
 	color:#6767db;
 	font-style:solid;
 	font-weight:700;
-	border:10px solid #dadada;
-	background-color: #dadada;
+	border:7px solid #e5e5e5;
+	background-color:#e5e5e5;
 	border-radius:20px;
 	height:min-content;
-	padding: 0px 15px;
+	padding: 0px 13px;
 `
 
 export const TabSecStyle = styled.div`
